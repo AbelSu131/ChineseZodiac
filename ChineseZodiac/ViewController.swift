23a02d7bd9c3c174f062afc8ab063b244eb00738
AbelSu <abel@abeldeMacBook-Pro.local>
2015-08-28 22:50:24 +0800

    十二生肖swift1.2
    输入出生年月即可知道你的属相

--- ChineseZodiac/ViewController.swift
+++ ChineseZodiac/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var yearOfBirth: UITextField!
+    @IBOutlet weak var image: UIImageView!
+    
+    let offset = 4//2008为鼠年，老鼠图片命名为0，2008%12=4
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        yearOfBirth.resignFirstResponder()//点击背景，数字键盘退出
+    }
+    @IBAction func okTapped(sender: AnyObject) {
+        yearOfBirth.resignFirstResponder()//输入框失去焦点时，数字键盘退出
+        
+        if let year = yearOfBirth.text.toInt(){
+            var imageNumber = (year - offset) % 12
+            image.image = UIImage(named: String(imageNumber))
+        }
+        else{
+            
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
